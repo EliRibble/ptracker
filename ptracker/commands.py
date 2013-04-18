7bc7b5f6733afa14c9bb9db2954373ff34154c51
Eli Ribble <eribble@hirevue.com>
2013-04-18 00:04:15 -0600

    Split out doing remote work into its own module
    This makes it easier to do commands that are compositions
    of remote work and DB work

--- ptracker/commands.py
+++ ptracker/commands.py
@@ -1,24 +1,8 @@
-import requests
-from lxml import etree
 import StringIO
-from ptracker.parser import parse
+from ptracker import remote
 from ptracker import db
 import logging
 
-PROJECTS_URL    = 'https://www.pivotaltracker.com/services/v3/projects'
-STORIES_URL     = 'https://www.pivotaltracker.com/services/v3/projects/{0}/stories'
-STORY_URL       = 'https://www.pivotaltracker.com/services/v3/projects/{0}/stories/{1}'
-
-ACTIVITIES_URL  = 'https://www.pivotaltracker.com/services/v4/stories/{0}/activities?limit=100'
-
-def _get_data(guid, url_pattern, parameters):
-    url = url_pattern.format(*parameters)
-    data = requests.get(url, headers={'X-TrackerToken': guid})
-    if not data.status_code == 200:
-        print(url, data.status_code, data.content)
-        raise Exception(data.content)
-    return data.content
-    
 def setupdb(db_user, db_password, db_name):
     try:
         db.setupdb(db_user, db_password, db_name)
@@ -28,21 +12,16 @@ def setupdb(db_user, db_password, db_name):
         return 1
     
 def projects(guid):
-    xml = _get_data(guid, PROJECTS_URL, ())
-    projects = parse(xml)
-    
+    projects = remote.get_projects(guid)
     for project in projects:
         print("Project {0}: {1}".format(project.name, project.id))
 
 def stories(guid, project_id):
-    stories = parse(_get_data(guid, STORIES_URL, (project_id,)))
+    stories = remote.get_stories(guid, project_id)
     logging.info("%s stories", len(stories))
 
 def story(guid, project_id, story_id):
-    if not story_id:
-        raise Exception("You must specify a story id")
-
-    story = parse(_get_data(guid, STORY_URL, (project_id, story_id)))
+    story = remote.get_story(guid, project_id, story_id)
 
     print("{0} {1} - {2}".format(story.story_type, story.id, story.name))
     print("Labels: {0}".format(story.labels))
@@ -60,10 +39,7 @@ def story(guid, project_id, story_id):
         print('"{0}"'.format(note.text))
 
 def activities(guid, story_id):
-    if not story_id:
-        raise Exception("You must specify a story id")
-
-    activities = parse(_get_data(guid, ACTIVITIES_URL, (story_id,)))
+    activities = remote.get_activities(guid, story_id)
     for activity in activities:
         logging.info("Activity {0} ({1}) - {2}".format(activity.id, activity.version, activity.event_type))
         #logging.info("Author: {0}".format(activity.author))
---
+++ ptracker/remote.py
@@ -0,0 +1,37 @@
+import requests
+from ptracker.parser import parse
+
+PROJECTS_URL    = 'https://www.pivotaltracker.com/services/v3/projects'
+STORIES_URL     = 'https://www.pivotaltracker.com/services/v3/projects/{0}/stories'
+STORY_URL       = 'https://www.pivotaltracker.com/services/v3/projects/{0}/stories/{1}'
+
+ACTIVITIES_URL  = 'https://www.pivotaltracker.com/services/v4/stories/{0}/activities?limit=100&page=2'
+
+def _get_data(guid, url_pattern, parameters):
+    url = url_pattern.format(*parameters)
+    data = requests.get(url, headers={'X-TrackerToken': guid})
+    if not data.status_code == 200:
+        print(url, data.status_code, data.content)
+        raise Exception(data.content)
+    return data.content
+    
+def get_projects(guid):
+    xml = _get_data(guid, PROJECTS_URL, ())
+    return parse(xml)
+
+def get_stories(guid, project_id):
+    xml = _get_data(guid, STORIES_URL, (project_id,))
+    return parse(xml)
+
+def get_story(guid, project_id, story_id):
+    if not story_id:
+        raise Exception("You must specify a story id")
+
+    return parse(_get_data(guid, STORY_URL, (project_id, story_id)))
+
+def get_activities(guid, story_id):
+    if not story_id:
+        raise Exception("You must specify a story id")
+
+    data = _get_data(guid, ACTIVITIES_URL, (story_id,))
+    return parse(data)
